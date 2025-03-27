c84956a9f7b2a97790b41c9b3e8db0058dde47c3
MEGAVATH-NAVEEN <naveenmegavath99126@gmail.com>
2025-03-27 17:35:31 +0530

    Add files via upload

---
+++ barrel.sv
@@ -0,0 +1,14 @@
+module barrel_bh(Out,In,Lr,n);
+
+ input [7:0]In;// input number
+ input [2:0]n;  // how many bits you want to shift
+ input Lr; // select the direction left ==1 otherwise right 
+ output reg [7:0]Out; // output result
+
+ always@(*) begin
+  if(Lr)
+   Out = In << n;
+  else
+   Out = In >> n;
+ end 
+endmodule
